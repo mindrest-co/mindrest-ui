65b01e1fe35e52967e9adcf25d484304b4a4e05b
sk97kang <sk97.kang@gmail.com>
2021-01-11 14:52:53 +0900

    Update CardList

--- src/components/CardList/CardList.stories.tsx
+++ src/components/CardList/CardList.stories.tsx
@@ -10,19 +10,32 @@ export default {
 
 const Template: Story<CardListProps> = args => <CardList {...args} />;
 
+const label = "2020. 12. 4일에 진행 예정된 검사";
+const items = [
+  {
+    image: "/assets/images/person.jpg",
+    title: "광화문숲 정신건강의학과",
+    subtitle: "이정석 선생님",
+    disabled: true,
+  },
+  {
+    image: "/assets/images/person.jpg",
+    title: "광화문숲 정신건강의학과",
+    subtitle: "이정석 선생님",
+  },
+];
+
 export const Default = Template.bind({});
 Default.args = {
-  label: "2020. 12. 4일에 진행 예정된 검사",
-  items: [
-    {
-      image: "/assets/images/person.jpg",
-      title: "광화문숲 정신건강의학과",
-      subtitle: "이정석 선생님",
-    },
-    {
-      image: "/assets/images/person.jpg",
-      title: "광화문숲 정신건강의학과",
-      subtitle: "이정석 선생님",
-    },
-  ],
+  label,
+  items,
+};
+
+export const Collaboration = () => {
+  return (
+    <>
+      <CardList label={label} items={items} />
+      <CardList label={label} items={items} />
+    </>
+  );
 };
