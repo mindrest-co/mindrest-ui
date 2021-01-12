82fb4776f1e2532e72a1392f23ecd600b007f39b
sk97kang <sk97.kang@gmail.com>
2021-01-12 17:30:22 +0900

    Update TextButton

--- src/components/TextButton/TextButton.stories.tsx
+++ src/components/TextButton/TextButton.stories.tsx
@@ -14,3 +14,13 @@ export default {
 const Template: Story<TextButtonProps> = args => <TextButton {...args} />;
 
 export const Default = Template.bind({});
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  icon: "plus",
+};
